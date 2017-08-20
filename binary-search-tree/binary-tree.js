32fd5c040a196b7b1f153d5be682cc4985f83da6
Nic Bonetto <nick.bonetto@gmail.com>
2017-08-20 09:30:48 -0700

    Declared push function. #1

--- binary-search-tree/binary-tree.js
+++ binary-search-tree/binary-tree.js
@@ -7,3 +7,26 @@ function Node(val) {
 function BinarySearchTree() {
   this.root = null
 }
+
+BinarySearchTree.prototype.push = function(val) {
+  const root = this.root
+
+  if (!root) {
+    this.root = new Node(val)
+    return
+  }
+
+  let currentNode = root
+  const newNode = new Node(val)
+
+  while (currentNode) {
+    if (val < currentNode.value) {
+      if (!currentNode.left) currentNode.left = newNode
+      else currentNode = currentNode.left
+    }
+    else {
+      if (!currentNode.right) currentNode.right = newNode
+      else currentNode = currentNode.right
+    }
+  }
+}
