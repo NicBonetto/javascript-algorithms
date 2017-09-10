bc63436166f80d8134a5a9b81291092b7eaffb6d
Nic Bonetto <nick.bonetto@gmail.com>
2017-09-10 11:39:08 -0700

    Declared modify method. #11

--- data-structures/hash-table.js
+++ data-structures/hash-table.js
@@ -32,12 +32,26 @@ HashTable.prototype.remove = function(key) {
 }
 
 HashTable.prototype.search = function(key) {
-  const message = {
-    'error': 'Sorry, that key was not found in this table.',
-    'found': 'Found ' + key + ' in the table.'
-  }
   const hash = this.computeHash(key)
+  const message = {
+    'error': 'Error, ' + key + ' was not found in this table.',
+    'found': key + ': ' + this.values[hash][key]
+  }
 
   if(this.values.hasOwnProperty(hash) && this.values[hash].hasOwnProperty(key)) return message['found']
+  else return message['error']
+}
+
+HashTable.prototype.modify = function(key, newVal) {
+  const hash = this.computeHash(key)
+  const message = {
+    'error': 'Error, ' + key + ' was not found in this table.',
+    'found': 'Changed ' + key + ' to have the value: ' + this.values[hash][key]
+  }
+
+  if(this.values.hasOwnProperty(hash) && this.values[hash].hasOwnProperty(key)) {
+    this.values[hash][key] = newVal
+    return message['found']
+  }
   else return message['error']
 }
