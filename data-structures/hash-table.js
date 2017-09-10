224875c8faed076fe9f8ed87509960c0c8706d1b
Nic Bonetto <nick.bonetto@gmail.com>
2017-09-10 10:58:24 -0700

    Declared computeHash function. #11

--- data-structures/hash-table.js
+++ data-structures/hash-table.js
@@ -1,8 +1,13 @@
-function HashTable() {
+function HashTable(size) {
   this.values = {}
   this.numberOfValues = 0
+  this.maxSize = size
 }
 
 HashTable.prototype.length = function() {
   return this.numberOfValues
+}
+
+HashTable.prototype.computeHash = function(key) {
+  return key.toString().length % this.maxSize
 }
