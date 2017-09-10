8df5574172d003103ad35a8db92b0b1b73847f68
Nic Bonetto <nick.bonetto@gmail.com>
2017-09-10 11:05:12 -0700

    Declared insert method. #11

--- data-structures/hash-table.js
+++ data-structures/hash-table.js
@@ -11,3 +11,13 @@ HashTable.prototype.length = function() {
 HashTable.prototype.computeHash = function(key) {
   return key.toString().length % this.maxSize
 }
+
+HashTable.prototype.insert = function(key, value) {
+  const hash = this.computeHash(key)
+
+  if(!this.values.hasOwnProperty(hash)) this.values[hash] = {}
+  if(!this.values[hash].hasOwnProperty(key)) {
+    this.values[hash][key] = value
+    this.numberOfValues++
+  }
+}
