4dc59d84fe34027138742b96ad482fcccd33c464
Nic Bonetto <nick.bonetto@gmail.com>
2017-09-23 16:26:13 -0700

    Created fibonacci memoized function. #15

---
+++ memoize/fibonacci.js
@@ -0,0 +1,12 @@
+function fibonacci(index, cache) {
+    cache = cache || []
+
+    if(cache[index]) return cache[index]
+    else {
+        if(index < 3) return 1
+        else {
+            cache[index] = fibonacci(index - 1, cache) + fibonacci(index - 2, cache)
+        }
+    }
+    return cache[index]
+}
